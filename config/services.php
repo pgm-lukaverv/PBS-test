e9fe2996dd225d17e6c7cee3de68d669978b2bf6
pgm-lukaverv <lukasvervaet.dev@outlook.com>
2026-09-09 14:09:31 +0200

    add OpenExchangeRates API key configuration to .env.example and services.php

--- config/services.php
+++ config/services.php
@@ -14,6 +14,10 @@ return [
     |
     */
 
+    'openexchangerates' => [
+        'key' => env('OPENEXCHANGERATES_API_KEY'),
+    ],
+
     'postmark' => [
         'key' => env('POSTMARK_API_KEY'),
     ],
